e723b460e8951aa3cfaefa3854b6fa22b24b62a7
tehmou <timo.tuominen@gmail.com>
2016-07-29 11:10:31 +0300

    Add separate layout for the fan item

---
+++ chapter8/FanExample/app/src/main/java/com/tehmou/fanexample/FanItem.java
@@ -0,0 +1,16 @@
+package com.tehmou.fanexample;
+
+/**
+ * Created by ttuo on 29/07/16.
+ */
+public class FanItem {
+    private final String title;
+
+    public FanItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
