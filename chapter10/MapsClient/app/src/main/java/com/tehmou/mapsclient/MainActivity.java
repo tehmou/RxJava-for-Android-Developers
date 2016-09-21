61a76bcbd35aa2969f3b97b67afab72fbed314e3
tehmou <timo.tuominen@gmail.com>
2016-09-21 14:55:04 +0100

    Process tiles with rx

--- chapter10/MapsClient/app/src/main/java/com/tehmou/mapsclient/MainActivity.java
+++ chapter10/MapsClient/app/src/main/java/com/tehmou/mapsclient/MainActivity.java
@@ -3,9 +3,13 @@ package com.tehmou.mapsclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jakewharton.rxbinding.view.RxView;
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collection;
+
+import rx.Observable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TilesView tilesView = (TilesView) findViewById(R.id.tiles_view);
-        final Collection<DrawableTile> drawableTiles =
+        final Collection<Tile> tiles =
                 Arrays.asList(
-                        new DrawableTile(0, 0, 0, 0, 256, 256),
-                        new DrawableTile(1, 0, 256, 0, 256, 256),
-                        new DrawableTile(0, 1, 0, 256, 256, 256),
-                        new DrawableTile(1, 1, 256, 256, 256, 256)
+                        new Tile(0, 0),
+                        new Tile(1, 0),
+                        new Tile(0, 1),
+                        new Tile(1, 1)
                 );
-        tilesView.setTiles(drawableTiles);
+
+        final int TILE_SIZE = 256;
+        Observable.from(tiles)
+                .map(tile ->
+                        new DrawableTile(tile,
+                                tile.getX() * TILE_SIZE,
+                                tile.getY() * TILE_SIZE,
+                                TILE_SIZE, TILE_SIZE))
+                .toList()
+                .subscribe(tilesView::setTiles);
     }
 }
