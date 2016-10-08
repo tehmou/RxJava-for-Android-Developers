27748071fcf2c307e8f7c0f1a616756846b47987
tehmou <timo.tuominen@gmail.com>
2016-10-08 16:43:35 +0300

    Add initial zoom in and zoom out

--- chapter12/MapsClient/app/src/main/java/com/tehmou/mapsclient/MainActivity.java
+++ chapter12/MapsClient/app/src/main/java/com/tehmou/mapsclient/MainActivity.java
@@ -4,8 +4,8 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.util.Pair;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.tehmou.mapsclient.network.MapNetworkAdapter;
 import com.tehmou.mapsclient.network.MapNetworkAdapterSimple;
 import com.tehmou.mapsclient.network.NetworkClient;
@@ -15,6 +15,7 @@ import com.tehmou.mapsclient.utils.CoordinateProjection;
 import com.tehmou.mapsclient.utils.LatLng;
 import com.tehmou.mapsclient.utils.MapTileUtils;
 import com.tehmou.mapsclient.utils.PointD;
+import com.tehmou.mapsclient.utils.Triple;
 import com.tehmou.mapsclient.utils.ViewUtils;
 
 import rx.Observable;
@@ -41,7 +42,11 @@ public class MainActivity extends AppCompatActivity {
         tilesView.setOnTouchListener(touchDelta);
         tilesView.setTileBitmapLoader(tileBitmapLoader);
 
-        final int zoom = 10;
+        BehaviorSubject<Integer> zoom = BehaviorSubject.create(10);
+        RxView.clicks(findViewById(R.id.zoom_in_button))
+                .subscribe(ignore -> zoom.onNext(zoom.getValue() + 1));
+        RxView.clicks(findViewById(R.id.zoom_out_button))
+                .subscribe(ignore -> zoom.onNext(zoom.getValue() - 1));
 
         Observable<PointD> tilesViewSize = tilesView.getViewSize();
         BehaviorSubject<LatLng> mapCenter =
@@ -50,30 +55,32 @@ public class MainActivity extends AppCompatActivity {
                 new CoordinateProjection((int) TILE_SIZE);
         Observable<PointD> offset =
                 Observable.combineLatest(
-                        tilesViewSize, mapCenter,
-                        (tilesViewSizeValue, mapCenterValue) ->
+                        tilesViewSize, mapCenter, zoom,
+                        (tilesViewSizeValue, mapCenterValue, zoomValue) ->
                                 MapTileUtils.calculateOffset(coordinateProjection,
-                                        zoom, tilesViewSizeValue, mapCenterValue));
+                                        zoomValue, tilesViewSizeValue, mapCenterValue));
 
+        Observable<Triple<PointD, PointD, Integer>> mapState =
+                Observable.combineLatest(
+                        tilesViewSize, offset, zoom, Triple::new);
         touchDelta.getObservable()
-                .withLatestFrom(
-                        Observable.combineLatest(tilesViewSize, offset, Pair::new),
-                        (pixelDelta, pair) -> {
+                .withLatestFrom(mapState,
+                        (pixelDelta, mapStateValue) -> {
                             Log.v(TAG, "pixelDelta(" + pixelDelta + ")");
-                            final double cx = pair.first.x / 2.0 - pair.second.x;
-                            final double cy = pair.first.y / 2.0 - pair.second.y;
+                            final double cx = mapStateValue.first.x / 2.0 - mapStateValue.second.x;
+                            final double cy = mapStateValue.first.y / 2.0 - mapStateValue.second.y;
                             final PointD newPoint = new PointD(cx - pixelDelta.x, cy - pixelDelta.y);
-                            return coordinateProjection.fromPointToLatLng(newPoint, zoom);
+                            return coordinateProjection.fromPointToLatLng(newPoint, mapStateValue.third);
                         })
                 .subscribe(mapCenter::onNext);
 
 
         Observable.combineLatest(
-                tilesViewSize,
-                offset,
-                (tilesViewSizeValue, offsetValue) -> MapTileUtils.calculateMapTiles(
-                        TILE_SIZE, zoom, tilesViewSizeValue, offsetValue
-                ))
+                tilesViewSize, offset, zoom,
+                (tilesViewSizeValue, offsetValue, zoomValue) ->
+                        MapTileUtils.calculateMapTiles(
+                                TILE_SIZE, zoomValue, tilesViewSizeValue, offsetValue
+                        ))
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(tilesView::setTiles);
     }
